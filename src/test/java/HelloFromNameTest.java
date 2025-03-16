812848e558c1fb6a9ce990bf00e66c033776e2a0
user <podgornaya.spb@gmail.com>
2025-03-16 22:30:17 +0300

    HelloWorldTest

---
+++ src/test/java/HelloFromNameTest.java
@@ -0,0 +1,10 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloFromNameTest {
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello from Lena");
+    }
+}
