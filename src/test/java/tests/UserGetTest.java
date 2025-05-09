81f137d59219a8bfc9b14685c4d933c097ce7e22
podgoele <podgornaya.spb@gmail.com>
2025-05-09 18:03:10 +0300

    Add files via upload

---
+++ src/test/java/tests/UserGetTest.java
@@ -0,0 +1,88 @@
+package tests;
+
+import io.qameta.allure.Issue;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.TmsLink;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.ApiCoreRequests;
+import lib.Assertions;
+import lib.BaseTestCase;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserGetTest extends BaseTestCase {
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+    @Test
+    public void testGetUserDataNotAuth() {
+        Response responseUserData = RestAssured
+                .get(apiCoreRequests.getBaseUrl() + "2")
+                .andReturn();
+
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotField(responseUserData, "firstName");
+        Assertions.assertJsonHasNotField(responseUserData, "lastname");
+        Assertions.assertJsonHasNotField(responseUserData, "email");
+    }
+
+    @Test
+    @TmsLink("User_Get_1")
+    public void testGetUserDetailsAuthAsSameUser() {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post(apiCoreRequests.getBaseUrl() + "login")
+                .andReturn();
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        Response responseUserData = RestAssured
+                .given()
+                .header("x-csrf-token", header)
+                .cookie("auth_sid", cookie)
+                .get(apiCoreRequests.getBaseUrl() + "2")
+                .andReturn();
+
+        String[] expectedFields = {"username", "firstName", "lastName", "email"};
+        Assertions.assertJsonHasFields(responseUserData, expectedFields);
+
+    }
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    public void testGetUserDetailsAuthAsOtherUser() {
+
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post(apiCoreRequests.getBaseUrl() + "login")
+                .andReturn();
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        Response responseUserData = RestAssured
+                .given()
+                .header("x-csrf-token", header)
+                .cookie("auth_sid", cookie)
+                .get(apiCoreRequests.getBaseUrl() + "1")
+                .andReturn();
+
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotField(responseUserData, "firstName");
+        Assertions.assertJsonHasNotField(responseUserData, "lastName");
+        Assertions.assertJsonHasNotField(responseUserData, "email");
+    }
+}
