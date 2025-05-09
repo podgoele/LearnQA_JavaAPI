3ea1de463aef81a8e8996d279fa017f75145418b
podgoele <podgornaya.spb@gmail.com>
2025-05-09 18:02:10 +0300

    Add files via upload

---
+++ src/test/java/tests/UserRegisterTest.java
@@ -0,0 +1,85 @@
+package tests;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import lib.ApiCoreRequests;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRegisterTest extends BaseTestCase {
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
+    @Test
+    public void testCreateUserWithExistingEmail() {
+        String email = "vinkotov@example.com";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post(apiCoreRequests.getBaseUrl())
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
+    }
+
+    @Test
+    @Severity(SeverityLevel.BLOCKER)
+    @Owner("ivanov")
+    public void testCreateUserSuccessfully() {
+        String email = DataGenerator.getRandomEmail();
+
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post(apiCoreRequests.getBaseUrl())
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
+        Assertions.assertJsonHasField(responseCreateAuth, "id");
+    }
+
+    @Test
+    public void testCreateUserWithInvalidEmail() {
+        String email = "invalidemail.com";
+        Response response = apiCoreRequests.createUserWithEmail(email);
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response, "Invalid email format");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"email", "password", "username", "firstName", "lastName"})
+    public void testCreateUserWithoutRequiredField(String field) {
+        Response response = apiCoreRequests.createUserWithoutField(field);
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response, "The following required params are missed: " + field);
+    }
+
+    @Test
+    public void testCreateUserWithVeryShortName() {
+        Response response = apiCoreRequests.createUserWithShortName();
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response, "The value of 'firstName' field is too short");
+    }
+
+    @Test
+    public void testCreateUserWithVeryLongName() {
+        Response response = apiCoreRequests.createUserWithLongName();
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response, "The value of 'firstName' field is too long");
+    }
+}
